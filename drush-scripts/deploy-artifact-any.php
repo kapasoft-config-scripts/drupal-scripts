380fd80f28b38b99c2556a516524acd419b46517
Margots Kapacs <makapacs@com.com>
2014-07-12 17:53:37 -0500

    adding stage for deploy any artifact

--- drush-scripts/deploy-artifact-any.php
+++ drush-scripts/deploy-artifact-any.php
@@ -9,6 +9,7 @@ $type = drush_get_option('artifact-type','module');
 $artifact_name = 'designssquare_com_'.$type.'_'.$name;
 $ver = drush_get_option('ver','0.1');
 $plus_package = drush_get_option('plus', 'no');
+$env = drush_get_option('env', 'dev');
 
 if($plus_package == 'yes'){
     $package_name = 'designssquare-com-'.$type.'-'.$name.'-plus';
@@ -33,7 +34,7 @@ if($min == 'yes'){
 drush_print('building '.$type.'s ...sudo cp -Rv '.$dist_package.'/'.$type.'s/ '.$test_artifact_dir.'/sites/all/'.$type.'s/');
 
 //delete existing code
-if ($handle = opendir($dist_package.'/modules/')) {
+if ($env == 'dev' && $handle = opendir($dist_package.'/modules/')) {
     while (false !== ($module = readdir($handle))) {
 
         if($module !== '.' && $module !== '..'){
@@ -45,16 +46,25 @@ if ($handle = opendir($dist_package.'/modules/')) {
         // note that '.' and '..' is returned even
     }
     closedir($handle);
+}elseif($env == 'stage'){
+    //ToDo
 }
 
 drush_print('building new modules....');
-drush_shell_exec('sudo cp -R '.$dist_package.'/modules/ '.$test_artifact_dir.'/sites/all/modules/');
+if($env == "stage"){
+    $dest_dir = 'sites/all/modules/'.get_widget_name($name, 'widget');
+    $git_repo = get_repo($name,'widget');
+    drush_shell_exec('git clone '.$git_repo.' '.$dest_dir);
+    print_r(drush_shell_exec_output());
+}else{
+    drush_shell_exec('sudo cp -R '.$dist_package.'/modules/ '.$test_artifact_dir.'/sites/all/modules/');
+}
 print_r(drush_shell_exec_output());
 
 /******THEMES******/
 if($type == 'theme'){
     //delete existing code
-    if ($handle = opendir($dist_package.'/themes/')) {
+    if ($env == 'dev' && $handle = opendir($dist_package.'/themes/')) {
         while (false !== ($theme = readdir($handle))) {
             if($theme !== '.' && $theme !== '..'){
                 drush_print('deleting existing theme:'.$theme.'....');
@@ -65,10 +75,20 @@ if($type == 'theme'){
             // note that '.' and '..' is returned even
         }
         closedir($handle);
+    }elseif($env == 'stage'){
+        //@ToDo
     }
 
+
     drush_print('building new themes....');
-    drush_shell_exec('sudo cp -R '.$dist_package.'/themes/ '.$test_artifact_dir.'/sites/all/themes/');
+    if($env == 'stage'){
+        $dest_dir = 'sites/all/themes/'.get_widget_name($name, 'theme');
+        $git_repo = get_repo($name,'theme');
+        drush_shell_exec('git clone '.$git_repo.' '.$dest_dir);
+        print_r(drush_shell_exec_output());
+    }else{
+        drush_shell_exec('sudo cp -R '.$dist_package.'/themes/ '.$test_artifact_dir.'/sites/all/themes/');
+    }
     print_r(drush_shell_exec_output());
 }
 
